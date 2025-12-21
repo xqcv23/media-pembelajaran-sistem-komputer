7d2824d6e77e68cd42ec39a73684343b46cc8306
xqcv23 <msaefudin112@gmail.com>
2025-12-22 00:23:55 +0800

    baru

---
+++ sistem-komputer/script.js
@@ -0,0 +1,13 @@
+function switchTab(evt, tabName) {
+    const wrapper = evt.target.closest('.modal-body-wrapper') || document; 
+    wrapper.querySelectorAll('.content-section').forEach(c => c.classList.remove('active'));
+    wrapper.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
+    const targetContent = wrapper.querySelector('#' + tabName);
+    if(targetContent) targetContent.classList.add('active');
+    evt.currentTarget.classList.add('active');
+  }
+
+  function resizeIframe(iframe) {
+      iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+      iframe.style.width = "100%";
+  }
